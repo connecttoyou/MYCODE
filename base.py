77e471a60e9cde2581e15c642ec2d490dac10171
connecttoyou <15673704627@wo.cn>
2020-03-02 17:30:51 +0800

    新加入从订单汇总表到数据库，以及从数据库查询的代码（20200302）

--- base.py
+++ base.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 class PO():
     def __init__(self):
         pass
@@ -26,14 +30,14 @@ class DB():
 
     def  setvalue(self,**kwargs):
         self.调拨单号 = kwargs['调拨单号']
-        self.订单编号 = kwargs['订单编号']
+        self.订单号 = kwargs['订单号']
         self.调出项目 = kwargs['调出项目']
         self.调入项目 = kwargs['调入项目']
         self.调拨日期 = kwargs['调拨日期']
         self.数量 = kwargs['数量']
         self.单价 = kwargs['单价']
         self.金额 = kwargs['金额']
-
+    def
 
 
 class DBResult():
@@ -50,10 +54,87 @@ class DBResult():
 
     def __len__(self):
         return len(self._订单信息)
-# po=PO()
-# po.setvalue(订单编号='123',商品名称='123',项目编号='123',项目名称='123',订单日期='123',支出类型='123',供应商='123',数量='123',单价='123',金额='123',税额='123',价税合计='123')
-# dbr=DBResult()
-# dbr.setXM(项目信息='123')
-# dbr.setPO(po)
-# print(len(dbr))
+
+class GetDB():
+    def __init__(self):
+        self._cookie={}
+        self._DBInfo=[]
+        self._headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+
+    def cookie(self,cookies):
+        for line in cookies.split(';'):# 把cookie字符串处理成字典，以便接下来使用
+            key, value = line.split('=', 1)
+            self._cookie[key] = value
+    # @cookie.setter
+    # def cookie(self):
+    #     return self._cookie
+
+    #从给定的链接获取所有需要爬取的链接
+    def url(self,url):
+        DBlink_list=[]
+        resp = requests.get(url, headers=self._headers, cookies=self._cookie)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        span = soup.find_all('b')
+        page = int(span[len(span) - 1].contents[0])#获得总页数
+        # page=1
+        for page in range(1,page+1):
+            newurl=url.replace('currentPage=1','currentPage=%s'%page)#第page页的链接
+            try:
+                resp = requests.get(newurl, headers=self._headers, cookies=self._cookie)
+                soup = BeautifulSoup(resp.text, 'lxml')
+                for idx, tr in enumerate(soup.find_all('tr')):
+                    if idx >= 3:
+                        tds = tr.find_all('td')
+                        # print(tds)
+                        if ((len(tds) >= 6)):# & (len(tds[6]) == 3)):
+                            tls = tds[0].contents[1]
+                            yield {
+                                    '调拨单号': tls.contents[0],  # 获得调拨单编号
+                                    'link':tls.attrs['href'], # 获取订单调拨单链接
+                                    '调出项目': tds[1].contents[0].replace("\r\n",'').replace("\t",'').replace(' ',''),
+                                    '调入项目': tds[2].contents[0].replace("\r\n",'').replace("\t",'').replace(' ',''),
+                                    '调拨日期':tds[5].contents[0].replace("\r\n",'').replace("\t",'').replace(' ','')
+
+                                }
+                        else:
+                            continue
+                    # return DBlink_list
+            except:
+                with Exception as ex:
+                    print(ex)
+
+    def getDBinfo(self,info):
+        try:
+            # for info in self._DBlink_list:
+                # info=self._DBlink_list[0]
+                resp = requests.get(info['link'], headers=self._headers, cookies=self._cookie)
+                soup = BeautifulSoup(resp.text, 'lxml')
+                trs=soup.find_all("tr")
+                info.update({"订单号":trs[1].text[trs[1].text.find('PO'):trs[1].text.find('调')].replace("\n",'').replace("\r",'').replace(" ",'')})
+                ss=trs[10].contents[7].text.replace(" ", '').replace("\r\n\t", '').replace("￥", '')
+                info.update({"金额": float(ss)})
+                ss=trs[10].contents[3].text.replace(" ",'').replace("\r\n\t",'')
+                info.update({'数量':int(ss)})
+                ss = trs[10].contents[5].text.replace(" ", '').replace("\r\n\t", '').replace("￥", '')
+                info.update({'单价':float(ss)})
+                # for tr in trs:
+                    # if tr.text.find('调拨源')>0 :#将调拨源加入结果
+                    #     # print(tr.text[tr.text.find('DB'):tr.text.find('预')].replace("\n",''))
+                    #     info.update({"调拨源":tr.text[tr.text.find('DB'):tr.text.find('预')].replace("\n",'')})
+                    #     self._DBInfo.append(info)
+                    # if tr.text.find('调拨含税金额')>0 :#将调拨金额加入结果
+                    #     s=str(tr.text.replace("\n\n\n",',').replace("\n",'').replace("￥",''))
+                    #     ss=s.split(',')
+                    #     ss[0].split(':')
+                    #     info.update({(ss[0].split(':'))[0]:float((ss[0].split(':'))[1]),(ss[1].split(':')[0]):float((ss[1].split(':'))[1])})
+                    #     if '调拨源' in info.keys():
+                    #         break
+
+                    # print("*********************************")
+                info.pop('link')
+                return info
+        except:
+            with Exception as ex:
+                print(ex)
+
 
--- oprate.py
+++ oprate.py
@@ -1,17 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
-from http import cookiejar
-from urllib import request
-import sys
-import io
 import xlwings as xw
-import base
+from base import PO,DBResult,GetDB,DB
 import pymongo
 
 
 def getPofromxls(path='')  :  # 从excel文件中获取订单信息
-    po=base.PO()
-    PoInfo=[]
+    po=PO()
     try:
         app = xw.App(visible=False, add_book=False)
         wb = app.books.open(path)
@@ -38,9 +31,9 @@ def getPofromxls(path='')  :  # 从excel文件中获取订单信息
 class ToDB():
     def __init__(self):
         try:
-            myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-            mydb = myclient["MssInfo"]
-            self._mydb=mydb["MssInfo"]
+            self._myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+            self._mydb=self._myclient["MssInfo"]["MssInfo"]
+
         except Exception as ex:
             print(ex)
 
@@ -51,18 +44,36 @@ class ToDB():
         self._mydb.insert_many(documents=value)
 
     def finddata(self,**kwargs):
-        dbresult=base.DBResult()
+        dbresult=DBResult()
         documents=self._mydb.find(kwargs)
         for document in documents:
-            po = base.PO()
+            po = PO()
             document.pop('_id')
             po.setvalue(**document)
             dbresult.setPO(po)
         dbresult.setXM(项目信息=kwargs['项目编号'])
         return  dbresult
 
+    def close(self):
+        self._myclient.close()
+
+
+cookie_str ='SESSION=50cb6054-39aa-430a-8444-ae02670dc123; ' \
+            'NTKF_T2D_CLIENTID=guestD39A76D9-9CB2-D451-1271-A1F19DB6A748;' \
+            ' LtpaToken=tQX78YHEEn1YBvIwIQdNNzNwQ8Sv9jiPOt7XKCqQnqRRmyaXBVcIDA2q67PZ2X9J4uQoBKnEtv96016BbU7hKthdAYTYzvYEL4e1cFDNZhixr1BYAZNUBekIU+i3Gbcy7AVAVCNyKsKLAEc6u+qOkFjrMNeJunE+ydkG5D72rfe2BuH7R2z5nVoZ8fZZnFgzH+WhiyqGTxrVS07YoVi5cPDfoq+DxGQr1VJSrAxZ6mGEiv9FoqkPPixaBEeR4PBJLNi2zZPcOvvyU7q6GF9FAB3vObByRF4MjR8KnTxlo+4=; ' \
+            'aiportalut=3IjwbmJf3u/93NgkqijMVh0JVZcfPDeTL0ZQgNzyiiU=; ' \
+            'nTalk_CACHE_DATA={uid:bl_1000_ISME9754_guestD39A76D9-9CB2-D4,tid:1583113893069262}'
+url='http://www.eshop.unicom.local:8080/eshop/newprojectChange/queryChangeOrder.do?current=16&shopCode=hnyiy&type=in&page.currentPage=1&pageChangeFlag=1'
 
 tdb=ToDB()
+db=DB()
+gdb=GetDB()
+gdb.cookie(cookie_str)
+DBlink_list=gdb.url(url)
 # for tl in getPofromxls('C:\\Users\\zx\\Downloads\\12.xls'):
 #     tdb.save_one(tl.getvalue())
-tdb.finddata(项目编号='HEC19AE0B00005',支出类型="01建筑工程投资")
+for link in gdb.url(url):
+    db.setvalue(**gdb.getDBinfo(link))
+    print('result')
+# tdb.finddata(项目编号='HEC19AE0B00005',支出类型="01建筑工程投资")
+tdb.close()
